4a35edeaad4d45de451b984dbac6b6e29672a0de
Diego Espinosa <espinosa9mx@gmail.com>
2025-04-17 18:09:53 -0600

    New Brach

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="about">
-      <h1>About Us</h1>
-      <p>Learn more about our company and mission.</p>
+    <div className="text-center">
+      <h1 className="text-4xl font-bold mb-6">About Us</h1>
+      <p className="text-xl text-gray-600">Learn more about our company and mission.</p>
     </div>
   );
 };
