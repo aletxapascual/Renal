cbf36dcdd1d4b5d4f70b2bdec50b3ac59600978e
aletxapascual <aletxa.pasacual@gmail.com>
2025-04-23 00:05:45 -0600

    Slideshow About page

--- src/pages/Acerca.js
+++ src/pages/Acerca.js
@@ -3,11 +3,13 @@ import Container1 from '../containers/about/Container1';
 import Container2 from '../containers/about/Container2';
 import Container3 from '../containers/about/Container3';
 import Container4 from '../containers/about/Container4';
+import PhotoSlideshow from '../containers/about/PhotoSlideshow';
 
 const Acerca = () => {
   return (
     <div>
       <Container1 />
+      <PhotoSlideshow />
       <Container2 />
       <Container3 />
       <Container4 />
